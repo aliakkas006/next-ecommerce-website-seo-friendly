aa3d9a7979d743750877c2bee2e9fd3cf1213881
Ali Akkas <aliakkas006@gmail.com>
2022-02-19 12:33:20 +0600

    navbar and product data added

---
+++ components/Layout.js
@@ -0,0 +1,43 @@
+import { AppBar, Container, Link, Toolbar, Typography } from '@material-ui/core';
+import Head from 'next/head';
+import NextLink from 'next/link';
+import React from 'react';
+
+import useStyles from '../utils/styles';
+
+const Layout = ({title, description, children }) => {
+  const classes = useStyles();
+
+  return (
+    <div>
+      <Head>
+        <title> {title ? `${title} - Next Amazona` : ' Next Amazona'}</title>
+        {description && <meta name='description' content={description}></meta>}
+      </Head>
+      <AppBar position="static" className={classes.navbar}>
+        <Toolbar>
+          <NextLink href="/" passHref>
+            <Link>
+              <Typography className={classes.brand}>amazona</Typography>
+            </Link>
+          </NextLink>
+          <div className={classes.grow}></div>
+          <div>
+            <NextLink href="/cart" passHref>
+              <Link>Cart</Link>
+            </NextLink>
+            <NextLink href="/login" passHref>
+              <Link>Login</Link>
+            </NextLink>
+          </div>
+        </Toolbar>
+      </AppBar>
+      <Container className={classes.main}>{children}</Container>
+      <footer className={classes.footer}>
+        All rights reserved. Next Amazona
+      </footer>
+    </div>
+  );
+};
+
+export default Layout;
